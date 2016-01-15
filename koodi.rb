b528e2ff8332ac3cff8ccfa10bc02b014f1f4d00
villetan <ville.tanskanen@helsinki.fi>
2016-01-15 16:20:47 +0200

    tehtava6 done

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
 # kirjoita koodi tänne
+def monista(taulukko)
+  taulu2 = taulukko.map do |alkio|
+    palautettava=""
+    alkio.times do
+      palautettava+="#{alkio}"
+    end
+  palautettava
+  end
+end

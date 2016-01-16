cc02f5fd4833692bb84b0c22e275394c1d4975ae
eski <esa@esa.com>
2016-01-17 00:41:32 +0200

    mooro

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
 # kirjoita koodi tänne
+
+def monista(taulu)
+  def kirjainsetti(numba)
+    teksti = ""
+    numba.times do
+      teksti = teksti + numba.to_s
+    end
+    return teksti
+  end
+  taulu2 = taulu.map do |luku|
+    kirjainsetti(luku)
+  end
+end

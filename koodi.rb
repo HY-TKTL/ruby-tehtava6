83dd66c2e766db6893382fcc02b9e2ac0a92699e
Simo Soininen <simo.s.soininen@helsinki.fi>
2016-01-24 21:52:38 +0200

    teht6

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
 # kirjoita koodi tänne
+def monista(taulukko)
+	
+	
+		
+	#	taulukko.map{|luku| luku.to_s * luku}
+		
+	
+	#taulukko.each do |s|
+	#	print "#{s} "
+	#end
+	#puts " "
+	return taulukko.map{|luku| luku.to_s * luku}
+end
